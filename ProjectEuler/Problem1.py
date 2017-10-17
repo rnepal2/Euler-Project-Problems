08dcea0e9809c6d6277d2085e9ab25ea4d63850f
rnepal2 <nepalrabindra89@gmail.com>
2017-10-17 12:09:14 -0500

    minor update

--- ProjectEuler/Problem1.py
+++ ProjectEuler/Problem1.py
@@ -1,25 +1,17 @@
+
 import math
+import time
 
-def list_all_multiples(N):
-    init_list = []
-    for i in range(1, N):
-        if (i % 3 == 0)or(i % 5 == 0):
-            init_list.append(i)
-    return(init_list)
-
-    #print(init_list)
-
-def Sum_of_multiples():
-    req_list = list_all_multiples(1000)
-    n = len(req_list)
-    #print(n)
+# sum of the multiples of 3 or 5 below 1000
+def sum_of_multiples():
     total_sum = 0
-    for i in req_list:
-        total_sum += i
+    for i in range(1000):
+        if (i % 3 == 0) | (i % 5 == 0):
+            total_sum += i
     return total_sum
 
-print("The sum of the multiples of 3 and 5 upt0 1000 is: = ", Sum_of_multiples())
-
-
+start_time = time.time()
+print("The sum of the multiples of 3 or 5 below 1000 is: = ", sum_of_multiples())
+print("Time taken: ", time.time() - start_time)
 
 

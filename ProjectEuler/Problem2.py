beeacc03015b3bf3f6108f6585b4cb8df893b028
Rabindra Nepal <nepalrabindra89@gmail.com>
2019-01-05 23:14:25 -0600

    Update Problem2.py

--- ProjectEuler/Problem2.py
+++ ProjectEuler/Problem2.py
@@ -1,25 +1,25 @@
 import math
 
-def Fibonacci(n):
+def fibonacci(n):
     if n == 0:
         return 0
     elif n == 1:
         return 1
     else:
-        fib_num = Fibonacci(n-1) + Fibonacci(n-2)
+        fib_num = fibonacci(n-1) + fibonacci(n-2)
         return fib_num
 
 
-def list_of_Fib():
+def list_of_fib():
     fib_list = []
     for i in range(2,nterms+2):
-        fib_num = Fibonacci(i)
+        fib_num = fibonacci(i)
         fib_list.append(fib_num)
     return fib_list
 
 nterms = 10000
 
-list = list_of_Fib()
+list = list_of_fib()
 print(max(list))
 
 

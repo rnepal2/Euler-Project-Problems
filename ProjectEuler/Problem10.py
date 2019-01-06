b3e3952bb3e6357bb2614613f3a9b65d5f3923ec
Rabindra Nepal <nepalrabindra89@gmail.com>
2019-01-05 23:12:29 -0600

    Update Problem10.py

--- ProjectEuler/Problem10.py
+++ ProjectEuler/Problem10.py
@@ -3,19 +3,19 @@ import time
 
 start_time = time.time()
 
-def Check_Prime(N):
+def check_prime(N):
 		if all(N % i != 0 for i in range(2,N)):
 			return True
 		return False
 		
-def List_Primes(n):
+def list_primes(n):
 	prime_list = []
 	for i in range(2,n):
-		if Check_Prime(i) == True:
+		if check_prime(i) == True:
 			prime_list.append(i)
 	return prime_list
 		
-def Sum_Of_Primes(a_list):
+def sum_of_primes(a_list):
 	sum = 0
 	for i in a_list:
 		sum = sum + i
@@ -23,11 +23,9 @@ def Sum_Of_Primes(a_list):
 	
 	
 num = int(input("Enter a number upto which prime is to listed: "))
-#ans = List_Primes(num)
-#length = len(ans)
-#total_sum = Sum_Of_Primes(ans)
-#print("The required sum is: ", total_sum)
-
-print(Check_Prime(num))
-
+ans = list_primes(num)
+length = len(ans)
+total_sum = sum_of_primes(ans)
+print("The required sum is: ", total_sum)
+print(check_prime(num))
 print("The run time is: ", time.time() - start_time)

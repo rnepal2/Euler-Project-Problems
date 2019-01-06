38941d74eb4dbd1c15022aae28fdf2c7d91fc2a5
Rabindra Nepal <nepalrabindra89@gmail.com>
2019-01-05 23:13:36 -0600

    Update Problem13.py

--- ProjectEuler/Problem13.py
+++ ProjectEuler/Problem13.py
@@ -4,7 +4,7 @@ import csv
 
 start_time = time.time()
 
-def Make_List():
+def make_list():
 	with open('TheNumbers.txt', 'rb') as csvfile:
 		list_of_numbers = []
 		for row in csvfile:
@@ -12,15 +12,15 @@ def Make_List():
 			list_of_numbers.append(int(numbers[0]))
 		return list_of_numbers
 		
-def Sum_Of_Numbers(a_list):
+def sum_of_numbers(a_list):
 	sum = 0
 	for i in a_list:
 		sum += i
 	return sum
 
 		
-the_list = Make_List()
-ans = Sum_Of_Numbers(the_list)
+the_list = make_list()
+ans = sum_of_numbers(the_list)
 sum_string = str(ans)
 length = len(sum_string)
 

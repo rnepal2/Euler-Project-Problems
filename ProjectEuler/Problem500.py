e5b8a31f4b8d95d7973574b0de6d9b4e2c3dd837
Rabindra Nepal <nepalrabindra89@gmail.com>
2019-01-05 23:07:40 -0600

    Update Problem500.py

--- ProjectEuler/Problem500.py
+++ ProjectEuler/Problem500.py
@@ -5,38 +5,30 @@ from functools import reduce
 start_time = time.time()
 
 
-def Num_Of_Factors(n):    
+def num_of_factors(n):    
 	factors_list = set(reduce(list.__add__,([i, n//i] for i in range(1, int(n**0.5) + 1) if n % i == 0)))
 	return len(factors_list)
 
 
-def List_To_Check(n):
+def list_to_check(n):
 	list_of_numbers = []
 	for i in range(1000000, n):
 		list_of_numbers.append(i)
 	return list_of_numbers
 	
-def Num_With_MaxFactors(a_list):
+def num_with_maxfactors(a_list):
 	factors = []
 	for i in a_list:
-		factors.append(Num_Of_Factors(i))
+		factors.append(num_of_factors(i))
 	return max(factors)
 		
 	
 
 
 check_upto = int(input("Enter the number to check upto: "))
-list_of_nums = List_To_Check(check_upto)
+list_of_nums = list_to_check(check_upto)
 sorted_list = sorted(list_of_nums)
-ans = Num_With_MaxFactors(sorted_list)
+ans = num_with_maxfactors(sorted_list)
 print("The maxmum number of factors are: ",ans)
 
-
-print(2**500500)
-
-
-
-
-
-	
 print("The total run time is: ", time.time() - start_time)
